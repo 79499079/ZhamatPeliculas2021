0724317a3c7f8a8c9921b9005dc523072714e73e
zhamat <zhamatsistemas@gmail.com>
2021-02-19 15:16:19 -0500

    varios

--- frontend/src/screens/SearchScreen.js
+++ frontend/src/screens/SearchScreen.js
@@ -1,21 +1,21 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { listProducts } from '../actions/productActions';
-import LoadingBox from '../components/LoadingBox';
-import MessageBox from '../components/MessageBox';
-import Product from '../components/Product';
-import Rating from '../components/Rating';
-import { precios, ratings } from '../utils';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { listProducts } from "../actions/productActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import Product from "../components/Product";
+import Rating from "../components/Rating";
+import { precios, ratings } from "../utils";
 
 export default function SearchScreen(props) {
   const {
-    name = 'all',
-    genero = 'all',
+    name = "all",
+    genero = "all",
     min = 0,
     max = 0,
     rating = 0,
-    order = 'newest',
+    order = "newest",
     pageNumber = 1,
   } = useParams();
   const dispatch = useDispatch();
@@ -32,8 +32,8 @@ export default function SearchScreen(props) {
     dispatch(
       listProducts({
         pageNumber,
-        name: name !== 'all' ? name : '',
-        genero: genero !== 'all' ? genero : '',
+        name: name !== "all" ? name : "",
+        genero: genero !== "all" ? genero : "",
         min,
         max,
         rating,
@@ -53,95 +53,35 @@ export default function SearchScreen(props) {
     return `/search/genero/${filterGenero}/name/${filterName}/min/${filterMin}/max/${filterMax}/rating/${filterRating}/order/${sortOrder}/pageNumber/${filterPage}`;
   };
   return (
-    <div>
-      <div className="row">
-        {loading ? (
-          <LoadingBox></LoadingBox>
-        ) : error ? (
-          <MessageBox variant="danger">{error}</MessageBox>
-        ) : (
-          <div>{products.length} Results</div>
-        )}
+    <div className="text-center">
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
         <div>
-          Sort by{' '}
-          <select
-            value={order}
-            onChange={(e) => {
-              props.history.push(getFilterUrl({ order: e.target.value }));
-            }}
-          >
-            <option value="newest">Newest Arrivals</option>
-            <option value="lowest">Precio: Low to High</option>
-            <option value="highest">Precio: High to Low</option>
-            <option value="toprated">Avg. Customer Reviews</option>
-          </select>
+          <h4>Resultados: {products.length}</h4>
         </div>
-      </div>
-      <div className="row top">
-        <div className="col-1">
-          <h3>Department</h3>
+      )}
+      <div className="row">
+        <div className="col-sm-12 col-md-3 col-lg-3">
           <div>
-            {loadingGeneros ? (
-              <LoadingBox></LoadingBox>
-            ) : errorGeneros ? (
-              <MessageBox variant="danger">{errorGeneros}</MessageBox>
-            ) : (
-              <ul>
-                <li>
-                  <Link
-                    className={'all' === genero ? 'active' : ''}
-                    to={getFilterUrl({ genero: 'all' })}
-                  >
-                    Any
-                  </Link>
-                </li>
-                {generos.map((c) => (
-                  <li key={c}>
-                    <Link
-                      className={c === genero ? 'active' : ''}
-                      to={getFilterUrl({ genero: c })}
-                    >
-                      {c}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-          <div>
-            <h3>Precio</h3>
-            <ul>
-              {precios.map((p) => (
-                <li key={p.name}>
-                  <Link
-                    to={getFilterUrl({ min: p.min, max: p.max })}
-                    className={
-                      `${p.min}-${p.max}` === `${min}-${max}` ? 'active' : ''
-                    }
-                  >
-                    {p.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div>
-            <h3>Avg. Customer Review</h3>
-            <ul>
-              {ratings.map((r) => (
-                <li key={r.name}>
-                  <Link
-                    to={getFilterUrl({ rating: r.rating })}
-                    className={`${r.rating}` === `${rating}` ? 'active' : ''}
-                  >
-                    <Rating caption={' & up'} rating={r.rating}></Rating>
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            Ordenar por{" "}
+            <select
+              value={order}
+              onChange={(e) => {
+                props.history.push(getFilterUrl({ order: e.target.value }));
+              }}
+            >
+              <option value="newest">Newest Arrivals</option>
+              <option value="lowest">Precio: Low to High</option>
+              <option value="highest">Precio: High to Low</option>
+              <option value="toprated">Avg. Customer Reviews</option>
+            </select>
           </div>
         </div>
-        <div className="col-3">
+
+        <div className="col-sm-12 col-md-9 col-lg-9">
           {loading ? (
             <LoadingBox></LoadingBox>
           ) : error ? (
@@ -151,15 +91,17 @@ export default function SearchScreen(props) {
               {products.length === 0 && (
                 <MessageBox>No Product Found</MessageBox>
               )}
-              <div className="row center">
-                {products.map((product) => (
-                  <Product key={product._id} product={product}></Product>
-                ))}
+              <div className="row">
+                
+                  {products.map((product) => (
+                    <Product key={product._id} product={product}></Product>
+                  ))}
+                
               </div>
-              <div className="row center pagination">
+              <div>
                 {[...Array(pages).keys()].map((x) => (
                   <Link
-                    className={x + 1 === page ? 'active' : ''}
+                    className={x + 1 === page ? "active" : ""}
                     key={x + 1}
                     to={getFilterUrl({ page: x + 1 })}
                   >
@@ -174,3 +116,93 @@ export default function SearchScreen(props) {
     </div>
   );
 }
+
+/* <div className="row">
+{loading ? (
+  <LoadingBox></LoadingBox>
+) : error ? (
+  <MessageBox variant="danger">{error}</MessageBox>
+) : (
+  <div>{products.length} Results</div>
+)}
+<div>
+  Sort by{' '}
+  <select
+    value={order}
+    onChange={(e) => {
+      props.history.push(getFilterUrl({ order: e.target.value }));
+    }}
+  >
+    <option value="newest">Newest Arrivals</option>
+    <option value="lowest">Precio: Low to High</option>
+    <option value="highest">Precio: High to Low</option>
+    <option value="toprated">Avg. Customer Reviews</option>
+  </select>
+</div>
+</div>
+<div className="row top">
+<div className="col-1">
+  <h3>Department</h3>
+  <div>
+    {loadingGeneros ? (
+      <LoadingBox></LoadingBox>
+    ) : errorGeneros ? (
+      <MessageBox variant="danger">{errorGeneros}</MessageBox>
+    ) : (
+      <ul>
+        <li>
+          <Link
+            className={'all' === genero ? 'active' : ''}
+            to={getFilterUrl({ genero: 'all' })}
+          >
+            Any
+          </Link>
+        </li>
+        {generos.map((c) => (
+          <li key={c}>
+            <Link
+              className={c === genero ? 'active' : ''}
+              to={getFilterUrl({ genero: c })}
+            >
+              {c}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+  <div>
+    <h3>Precio</h3>
+    <ul>
+      {precios.map((p) => (
+        <li key={p.name}>
+          <Link
+            to={getFilterUrl({ min: p.min, max: p.max })}
+            className={
+              `${p.min}-${p.max}` === `${min}-${max}` ? 'active' : ''
+            }
+          >
+            {p.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+  <div>
+    <h3>Avg. Customer Reviews</h3>
+    <ul>
+      {ratings.map((r) => (
+        <li key={r.name}>
+          <Link
+            to={getFilterUrl({ rating: r.rating })}
+            className={`${r.rating}` === `${rating}` ? 'active' : ''}
+          >
+            <Rating caption={' & up'} rating={r.rating}></Rating>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+</div>
+
+*/
